c5f0b3116847d30a621a3fbc82ec47bbfccc84fe
Esaul Alfacan <151419707+EsaulAlfacan@users.noreply.github.com>
2024-01-04 12:11:08 -0600

    VINCULACION CON GIT

---
+++ js/footer_contac.js
@@ -0,0 +1,4 @@
+document.getElementById('emailLink').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'mailto:grupoalfacan@gmail.com';
+  });
---
+++ page-grupo_alfacan/README.md
@@ -0,0 +1,2 @@
+# page-grupo_alfacan
+See this project in: https://grupoalfacan.website
